350f12802f272654d7fe8a54e474021c30ea78bd
Aanya <jainaanya2000@gmail.com>
2020-09-07 11:33:46 +0530

    editing done

--- App.js
+++ App.js
@@ -4,14 +4,37 @@ import Header from './components/Header';
 import StartGameScreen from './Screens/StartGameScreen';
 import GameScreen from './Screens/GameScreen';
 import GameOverScreen from './Screens/GameOverScreen';
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo';
+
+const fetchFonts=()=>{
+  return Font.loadAsync({
+    'open-sans':require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold':require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+}
 
 export default function App() {
   const [userNumber,setUserNumber]=useState();
   const [guessRounds,setGuessRounds]=useState(0);
+  const [dataLoaded,setDataLoaded]=useState(false);
+
+  if(!dataLoaded)
+  {
+    return (<AppLoading startAsync={fetchFonts}
+    onFinish={()=>setDataLoaded(true)}
+    onError={(err)=>console.log(err)}
+    />
+    );
+  }
+
+  const newGameHandler=props=>{
+    setGuessRounds(0);
+    setUserNumber(null);
+  }
 
   const startGameHandler=(selectedNumber)=>{
     setUserNumber(selectedNumber);
-    setGuessRounds(0);
   }
   const gameOverHandler=numOfRounds=>{
     setGuessRounds(numOfRounds);
@@ -20,14 +43,20 @@ export default function App() {
 
 
   let content=<StartGameScreen onStartGame={startGameHandler} />
-
+  content=(<GameOverScreen roundNum={1} 
+  userNum={1}
+  onRestart={newGameHandler}
+  />);
   if(userNumber && guessRounds<=0)
   {
     content=<GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
   }
   else if(guessRounds>0)
   {
-    content=<GameOverScreen />;
+    content=<GameOverScreen roundNum={guessRounds} 
+    userNum={userNumber}
+    onRestart={newGameHandler}
+    />;
   }
 
   return (
--- Screens/GameOverScreen.js
+++ Screens/GameOverScreen.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {View,Text,StyleSheet} from 'react-native';
+import {View,Text,StyleSheet,Button,Image} from 'react-native';
 
 const GameOverScreen=props=>{
     return (
         <View style={styles.screen}>
             <Text>The Game is over!!</Text>
+            <View style={styles.cont}>
+            {/* <Image source={require('../assets/gameover.png')} */}
+            <Image source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWUGhDXkgolW8CVthSMpAxo3UmrOw3OrJuzA&usqp=CAU'}}
+            style={styles.img}
+            resizeMode="cover"
+            />
+            </View>
+           <Text>The Number of Rounds: {props.roundNum}</Text>
+           <Text>Number was : {props.userNum}</Text>
+           <Button title="NEW GAME" onPress={props.onRestart} />
         </View>
     );
 };
@@ -14,7 +24,22 @@ const styles=StyleSheet.create({
         flex:1,
         justifyContent:'center',
         alignItems:'center'
+    },
+    cont:{
+     borderRadius:150,
+     borderWidth:2,
+     borderColor:'grey',
+     width:300,
+     height:300,
+     overflow:'hidden',
+     marginVertical:10
+    },
+    img:{
+        height:'100%',
+        width:'100%',
+
     }
+   
 });
 
 export default GameOverScreen;
